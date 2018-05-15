ee5c93c9785757a4613df7e8a3b5f24e1bf44eda
Costin Botez <costibotez94@gmail.com>
2018-05-15 15:48:01 +0100

    Setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env) => {
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         module: {
@@ -45,7 +45,8 @@ module.exports = (env) => {
         devtool: (isProduction) ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true // serve index.html everytime we have 404
+            historyApiFallback: true, // serve index.html everytime we have 404
+            publicPath: '/dist/'    // change everything to route to dist folder
         }
     }
 };
